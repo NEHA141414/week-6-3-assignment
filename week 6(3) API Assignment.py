7e324bc240f287392aab87ac7280ac1b2feda109
Neha Rana <135492525+NEHA141414@users.noreply.github.com>
2023-08-29 18:11:15 +0530

    API Assignment

---
+++ week 6(3) API Assignment.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Q-1 What is an API ? Give an example, where an API is used in real life.
+
+# Ans-1 API stands for application programming interface. It is an abbrevation for application programming interface which is a collection of communication protocols and subrout used by various programs to communicate between them.
+# A programmer can make use of various API tools to make their program easier and simpler.
+# 
+# real life example of API -
+#  1. Log in using xyz account
+#  2. weather snippets
+#  3. pay with paypal
+#  4. google maps
+#  5. travel booking
+
+# Q-2 Give advantages and disadvantages of using API.
+
+# Advantages API :
+#     
+# 1. Increased Interoperability : API solutions enable different software system to communicate with each other, regardless of        their programming language or platform.
+# 
+# 2. Improved Efficiency : API driven infrastructures enable developers to reuse  code and functionality from existing software
+#    system, saving time and reducing the amount of code that needs to be written from scatch.
+#    
+# 3. Increased flexibility : APIs provide developers with greater flexibility,
+#    enabling them to choose the best programming languages and frameworks for thier needs.
+#    
+# Disadvantage API :
+# 
+# 1. Security concerns : API infrastructures can be vulnerable to security breaches.
+# 
+# 2. complexity : APIs can be complex, requiring a significant amount of development version effort to implement and maintain.
+# 
+# 3. compatibility Issues : APIs can have compatibility issues between defferent versions of the same software system or between different system, which can lead to errors and system failures.
+
+# Q-3 What is a web API ? Differentiate between API and web API.
+
+# Ans-3 Web API is an API as the name suggests. it can be accessed over the web using the HTTP protocol. It is a framework that helps you to create and develops HTTP based RESTFUL service. The web API can be developed by using different technologies such as java ASP, NET , etc.
+# 
+# 
+# Difference between API and Web API :-
+# 
+# API can refers to any interface for communication between software component , system or application irrespactive of plateform or medium ,and web API specially designed for web based application involving data exchange over internet using standard technologies like HTTP.
+
+# Q-4 Explain REST and SOAP Architecture . Mention shortcoming of SOAP.
+
+# Ans-4 SOAP stands for simple object access protocol and REST stands for representational state transfer. 
+#       since SOAP is a protocol ,it follows a strict standard to allow communication between the client and the server whereas REST is an architectural style that does not follow any strict standard but follow six constraints defined by ROY Fielding in 2000. Those constraints are  Uniform Interface ,Client- server , Stateless , Cacheable, Layered system , code on demand.
+#       
+#       
+# Shortcoming of SOAP :-
+# 
+# 1. API calls are not cached :
+# An API call is a process of sending a request when an API has been set up with the necessary endpoints.
+# 
+# 2. Extremely comlicated :
+# SOAP is substantially more sophisticated than REST. It is also less adaptable.
+# 
+# 3. No Choice of data formate :
+# SOAP's support for data formate is likewise severely constrained . HTML,JSON , YAML,XML, and more formats are support .
+# SOAP ,on the other hand , only support XML.
+# 
+# 4. Requires more bandwidth :
+# SOAP is often slower than REST , and it also consumes more bandwidthdue to its complexity.
+
+# Q-5 Differentiate between REST and SOAP.
+
+# Ans-5 
+# 1.  SOAP is simpple object access protocol and REST is representational state transfer.
+# 2. SOAP os heavier and needs more bandwidth and REST is lightwieght and needs less bandwidth .
+# 3. SOAP defines its own security and rest inherit security from underlying transport.
+# 4. SOAP call c
